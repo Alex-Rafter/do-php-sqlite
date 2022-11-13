a02725a7428c34c571097f0ff2b739daf54615ba
Alex Rafter <alexrafteremail@gmail.com>
2022-11-13 16:57:36 +0000

    tweak

--- packages/db/write/index.php
+++ packages/db/write/index.php
@@ -4,7 +4,7 @@ function main(array $args): array
 {
 
     $argName = $args["name"] ?? "stranger";
-    $db = new PDO('./database/sqlite:sample.db');
+    $db = new PDO('../read/sqlite:sample.db');
     $db->exec("CREATE TABLE IF NOT EXISTS hello (id INTEGER PRIMARY KEY, name TEXT)");
     $db->exec("INSERT INTO hello (name) VALUES ('${argName}')");
     $result = $db->query('SELECT * FROM hello');
@@ -12,7 +12,7 @@ function main(array $args): array
 
     $db = NULL;
 
-
+    // https://docs.digitalocean.com/products/functions/how-to/structure-projects/
 
     return [
         'body' => $json,
